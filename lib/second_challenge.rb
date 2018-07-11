2727be8a7f94d3d2c07f51ab024332fedb8b5bbe
Scott Ungchusri <animoogle504@gmail.com>
2018-07-11 16:30:45 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => "Lord Montague"
-      :matriarch => "Lady Montague"
-      :hero => "Romeo"
-      :heros_friends => 
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => {}
       }, 
     :capulet => {
       #your key/value pairs here!
